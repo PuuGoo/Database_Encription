1d82571df3ff4d32e5a5d0eb367beaaecaa44f23
PuuGoo <ndphu1997@gmail.com>
2023-04-26 23:27:08 +0700

    Lever 6 Oauth Facebook Google....

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ import ejs from "ejs"
 import session from "express-session"
 import passport from "passport"
 import passportLocalMongoose from "passport-local-mongoose"
-
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
+import findOrCreate from "mongoose-findorcreate"
+import {Strategy as FacebookStrategy} from "passport-facebook"
 
 main().catch(function(err){
     console.log(err);
@@ -25,23 +27,66 @@ await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 console.log("Succesfully connected");
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+    //This is a monogo error. In your mongoose schema you maybe defined as username {type string, unique:true}. So when you are storing data in DB, only one null value for username will store. After that if username field has null value duplicate error will throw. Change username field as below
+    username: {
+        type: String,
+        index: true,
+        nique: true,
+        sparse: true,
+    },
+    password: String,
+    googleId: String,
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
 passport.serializeUser(function(user, done) {
-    done(null, user);
+    done(null, user._id);
 });
-  
-passport.deserializeUser(function(user, done) {
-    done(null, user);
+ 
+passport.deserializeUser(function(id, done) {
+  User.findById(id)
+  .then(function(user){
+    done(err, user);
+  })
+  .catch(function(err){
+    console.log(err);
+  });
 });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({username: profile.emails[0].value ,googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.APP_ID,
+    clientSecret: process.env.APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 const app = express();
 
@@ -64,11 +109,15 @@ app.get("/", function(req,res){
 });
 
 app.get("/secrets", function(req,res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
+    User.find({secret: {$ne: null}})
+    .then(function(foundUsers){
+        if(foundUsers){
+            res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+    })
+    .catch(function(err){
+        console.log(err);
+    });
 });
 
 app.get("/logout", function(req,res){
@@ -81,6 +130,55 @@ app.get("/logout", function(req,res){
     });
 
 });
+
+app.route("/submit")
+.get(function(req,res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+})
+.post(function(req,res){
+    const submittedSecret = req.body.secret;
+    
+    User.findById(req.user.id)
+    .then(function(foundUser){
+        if(foundUser){
+            foundUser.secret = submittedSecret;
+            foundUser.save()
+            .then(function(){
+                res.redirect("/secrets");
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+        }
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+});
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: [ "email", "profile" ] }
+));
+
+app.get( "/auth/google/secrets",
+passport.authenticate( 'google', {
+    successRedirect: "/secrets",
+    failureRedirect: "/login"
+}));
+
+app.get("/auth/facebook",
+    passport.authenticate('facebook'));
+
+app.get("/auth/facebook/secrets",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 
 app.route("/register")
 .get(function(req,res){
@@ -119,6 +217,7 @@ app.route("/login")
         }
     });
 });
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
